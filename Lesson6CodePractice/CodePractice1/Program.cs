4d6b6e56e15c488aba4e8b4fefd8e95f0029a937
vanhakobyan1996 <vanhakobyan1996@gmail.com>
2018-07-18 21:30:53 +0400

    add code practice

---
+++ Lesson6CodePractice/CodePractice1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodePractice1
+{
+    class Program
+    {
+        //text is contain only number
+        //Return that count item where contain list
+
+        public static int GetItem(string text, List<decimal> list) => list.Count(x => x == decimal.Parse(text));
+
+
+        static void Main(string[] args)
+        {
+            List<decimal> list = new List<decimal>();
+            Console.WriteLine("Please imput number");
+            string text = Console.ReadLine();
+            for (int i = 0; i < 100; i++)
+            {
+                list.Add(i);
+            }
+
+            var count = GetItem(text, list);
+            Console.WriteLine(count);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson6CodePractice/CodePractice2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodePractice2
+{
+    public class Person
+    {
+        public string name { get; set; }
+    }
+    class Program
+    {
+        public static void ChangeName(ref string name)
+        {
+            name = name.Substring(0, 2);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input any name");
+            string name = Console.ReadLine();
+            ChangeName(ref name);
+            Console.WriteLine(name);
+            Console.ReadKey();
+        }
+    }
+}
