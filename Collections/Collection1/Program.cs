0fae65fecf939532f41ee2978a6bd7570cbc4c4f
vanhakobyan <vanhakobyan1996@gmail.com>
2018-08-01 21:11:18 +0400

    add collections simple using

---
+++ Collections/Collection1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collection1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[37];
+            arr[0] = 125;
+            arr[36] = 15;
+            int[] more10 = arr.Where(x => x > 10).ToArray();
+
+
+            List<string> subList = new List<string>();
+            subList.Add("new item");
+
+            List<string> strList = new List<string>();
+
+            strList.Add("15");
+            strList.Add("16");
+            strList.Add("17");
+
+            strList.AddRange(subList);
+
+            foreach (var item in strList)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
