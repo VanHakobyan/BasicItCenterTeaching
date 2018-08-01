56fe7e52e1094efd202568c9c7764f9f6601b595
vanhakobyan <vanhakobyan1996@gmail.com>
2018-08-01 20:54:50 +0400

    add implementation webApi core

---
+++ Lesson8WebApi/Lesson8WebApi/BLL/CompanyMenagmant.cs
@@ -0,0 +1,41 @@
+﻿using Lesson8WebApi.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lesson8WebApi.BLL
+{
+    public class CompanyMenagmant
+    {
+        public List<MyModel> companies;
+        public CompanyMenagmant()
+        {
+            companies = new List<MyModel>();
+            SetCompanies();
+        }
+        private void SetCompanies()
+        {
+            var basicItCenter = new MyModel();
+
+            basicItCenter.Id = 1;
+            basicItCenter.CompanyName = "Basic IT Center";
+            basicItCenter.CompanyEmail = "basic@it-center.am";
+
+            companies.Add(basicItCenter);
+
+            var microsoft = new MyModel()
+            {
+                Id = 2,
+                CompanyName = "Microsoft",
+                CompanyEmail = "Microsoft@microsoft.com",
+            };
+
+            companies.Add(microsoft);
+
+            companies.Add(new MyModel { Id = 3, CompanyName = "Amazon", CompanyEmail = "amazon@amazon.com" });
+
+
+        }
+    }
+}
--- Lesson8WebApi/Lesson8WebApi/Controllers/ValuesController.cs
+++ Lesson8WebApi/Lesson8WebApi/Controllers/ValuesController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Lesson8WebApi.BLL;
+using Lesson8WebApi.Model;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Lesson8WebApi.Controllers
@@ -9,37 +11,60 @@ namespace Lesson8WebApi.Controllers
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
+        private static CompanyMenagmant companyMenagmant = new CompanyMenagmant();
         // GET api/values
         [HttpGet]
-        public IEnumerable<string> Get()
+        public List<MyModel> Get()
         {
-
-            return new string[] { "value1", "value2" };
+            return companyMenagmant.companies;
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public MyModel Get(int id)
         {
-            return "value";
+            //bad practice
+
+            //for (int i = 1; i <= companyMenagmant.companies.Count; i++)
+            //{
+            //    if (id == companyMenagmant.companies[i].Id)
+            //    {
+            //        return companyMenagmant.companies[i];
+            //    }
+            //}
+            //return null;
+
+            //good practice
+            return companyMenagmant.companies.FirstOrDefault(x => x.Id == id);
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody]string value)
+        public List<MyModel> Post([FromBody]MyModel myModel)
         {
+            companyMenagmant.companies.Add(myModel);
+            return companyMenagmant.companies;
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        public IActionResult Put(int id, [FromBody]MyModel myModel)
         {
+            var selectedModel = companyMenagmant.companies.FirstOrDefault(x => x.Id == id);
+            if (selectedModel is null) return BadRequest();
+            companyMenagmant.companies.Remove(selectedModel);
+            companyMenagmant.companies.Add(myModel);
+            return Ok(myModel);
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(int id)
         {
+            var selectedCompany = companyMenagmant.companies.FirstOrDefault(x => x.Id == id);
+            if (selectedCompany is null) return BadRequest();
+            companyMenagmant.companies.Remove(selectedCompany);
+            return Ok(selectedCompany);
         }
     }
 }
