e2675eaf40f0328d2a0c7cabcd90dcc767ffb340
vanhakobyan <vanhakobyan1996@gmail.com>
2018-08-01 21:32:33 +0400

    add delegate

---
+++ Delegate/Delegate/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    class Program
+    {
+        delegate int del(int i);
+
+        static void Main(string[] args)
+        {
+            del myDelegate = y => y * y;
+            del myDelegate2 = Mul;
+            int j = myDelegate(5);
+            Console.WriteLine(j);
+            Console.ReadLine();
+
+        }
+        public static int Mul(int i)
+        {
+            return i * i;
+        }
+    }
+}
