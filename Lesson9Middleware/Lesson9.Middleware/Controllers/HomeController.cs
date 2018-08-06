6a17dd6678e6fb2b2ba68a4b0a8fd15007fdd3db
vanhakobyan <vanhakobyan1996@gmail.com>
2018-08-06 19:58:40 +0400

    add WPF async and init middle...

---
+++ Lesson9Middleware/Lesson9.Middleware/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Lesson9.Middleware.Models;
+using System.Threading;
+
+namespace Lesson9.Middleware.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            Thread.Sleep(20000);
+            //Task.Run(() => );
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Lesson9Middleware/Lesson9.Middleware/Models/TokenMiddleware.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lesson9.Middleware.Models
+{
+    using Microsoft.AspNetCore.Http;
+    using System.Threading.Tasks;
+
+    public class TokenMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public TokenMiddleware(RequestDelegate next)
+        {
+            this._next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+         {
+            var token = context.Request.Query["token"];
+            if (token != "12345678")
+            {
+                context.Response.StatusCode = 403;
+                await context.Response.WriteAsync("Token is invalid");
+            }
+            else
+            {
+                await _next.Invoke(context);
+            }
+        }
+    }
+
+}
