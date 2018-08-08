4c74e1a001e1ab64e04e4171510edc60eda51e2a
vanhakobyan1996 <vanhakobyan1996@gmail.com>
2018-08-08 19:41:14 +0400

    EF core and nugate managmant

---
+++ NugetManagmant/NugetManagmant/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using SortingGeneric;
+
+namespace NugetManagmant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           List<Product> products=new List<Product>();
+            products.Add(new Product{Id = 1,Name = "Laptop",Price = 1500000});
+            products.Add(new Product{Id = 2,Name = "mouse",Price = 1500});
+            var serializeObject = JsonConvert.SerializeObject(products,Formatting.Indented);
+            Console.WriteLine(serializeObject);
+        }
+    }
+}
