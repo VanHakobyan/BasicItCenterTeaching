c568f3a752f920ab1fa6a8fc1c209119b5957cb0
vanhakobyan1996 <vanhakobyan1996@gmail.com>
2018-08-08 22:50:36 +0400

    add implemet...

--- CodePracticeLesson11/ArrayAndList/Program.cs
+++ CodePracticeLesson11/ArrayAndList/Program.cs
@@ -10,6 +10,21 @@ namespace ArrayAndList
     {
         static void Main(string[] args)
         {
+            int[] arr = new int[10];
+            List<int> list = new List<int>();
+            arr[0] = 1;
+            list.Add(1);
+            Console.WriteLine(arr[0]);
+            Console.WriteLine(arr.ElementAt(0));
+            Console.WriteLine(list[0]);
+            Console.WriteLine(list.ElementAt(0));
+
+            arr[0] = default(int);
+            list.RemoveAt(0);
+            Console.WriteLine(arr[0]);
+            //Console.WriteLine(list[0]);
+
+          
         }
     }
 }
